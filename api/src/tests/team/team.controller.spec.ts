781216851f25f5d9639351ee5af430a6992a0ea1
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-12-30 15:53:44 +0100

    fix(api): update team tests

---
+++ api/src/tests/team/team.controller.spec.ts
@@ -0,0 +1,86 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TeamController } from '../../team/team.controller';
+import { TeamService } from '../../team/team.service';
+import { CreateTeamDto } from '../../team/dto/create-team.dto';
+import { APIQuery } from '../../types/query.type';
+import { Team } from '../../team/entities/team.entity';
+
+jest.mock('../../team/team.service');
+
+describe('TeamController', () => {
+  let controller: TeamController;
+  let service: TeamService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TeamController],
+      providers: [TeamService],
+    }).compile();
+
+    controller = module.get<TeamController>(TeamController);
+    service = module.get<TeamService>(TeamService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should create a team', async () => {
+    const dto: CreateTeamDto = {
+      name: '',
+    };
+    let result: void;
+    jest.spyOn(service, 'create').mockResolvedValue(result);
+
+    expect(await controller.create(dto)).toBe(result);
+  });
+
+  it('should find all teams', async () => {
+    const query: APIQuery = {
+      /* fill with appropriate data */
+    };
+    const result: {
+      page: number;
+      limit: number;
+      count: number;
+      data: Team[];
+    } = {
+      page: 1,
+      limit: 10,
+      count: 0,
+      data: [],
+    };
+    jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+    expect(await controller.findAll(query)).toBe(result);
+  });
+
+  it('should find a team by id', async () => {
+    const id = 'testId';
+    const result: Team = {
+      id: '',
+      name: '',
+      users: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    jest.spyOn(service, 'findOneById').mockResolvedValue(result);
+
+    expect(await controller.findOneById(id)).toBe(result);
+  });
+
+  it('should find a team by name', async () => {
+    const name = 'testName';
+    const result: Team = {
+      id: '',
+      name: '',
+      users: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    jest.spyOn(service, 'findOneByName').mockResolvedValue(result);
+
+    expect(await controller.findOneByName(name)).toBe(result);
+  });
+});
---
+++ api/src/tests/team/team.service.spec.ts
@@ -0,0 +1,73 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TeamService } from '../../team/team.service';
+import { TeamRepository } from '../../team/team.repository';
+import { CreateTeamDto } from '../../team/dto/create-team.dto';
+import { UpdateTeamDto } from '../../team/dto/update-team.dto';
+import { Team } from '../../team/entities/team.entity';
+
+describe('TeamService', () => {
+  let service: TeamService;
+  let repo: TeamRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TeamService, { provide: TeamRepository, useValue: {} }],
+    }).compile();
+
+    service = module.get<TeamService>(TeamService);
+    repo = module.get<TeamRepository>(TeamRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should create a team', async () => {
+    const team: CreateTeamDto = {
+      name: '',
+    };
+    jest.spyOn(repo, 'createTeam').mockResolvedValueOnce(null);
+    await expect(service.create(team)).resolves.not.toThrow();
+  });
+
+  it('should find all teams', async () => {
+    const queries = {
+      /* fill with valid data */
+    };
+    jest.spyOn(repo, 'findAllTeams').mockResolvedValueOnce({
+      page: 1,
+      limit: 10,
+      count: 0,
+      data: [],
+    });
+    await expect(service.findAll(queries)).resolves.not.toThrow();
+  });
+
+  it('should find one team by id', async () => {
+    const id = 'some-id';
+    jest.spyOn(repo, 'findOneTeamById').mockResolvedValueOnce({} as Team);
+    await expect(service.findOneById(id)).resolves.not.toThrow();
+  });
+
+  it('should find one team by name', async () => {
+    const name = 'some-name';
+    jest.spyOn(repo, 'findOneTeamByName').mockResolvedValueOnce({} as Team);
+    await expect(service.findOneByName(name)).resolves.not.toThrow();
+  });
+
+  it('should update a team', async () => {
+    const id = 'some-id';
+    const team: UpdateTeamDto = {
+      name: '',
+    };
+    jest.spyOn(repo, 'updateTeam').mockResolvedValueOnce(undefined);
+    await expect(service.update(id, team)).resolves.not.toThrow();
+  });
+
+  it('should remove a team', async () => {
+    const id = 'some-id';
+    jest.spyOn(repo, 'deleteTeam').mockResolvedValueOnce(undefined);
+    await expect(service.remove(id)).resolves.not.toThrow();
+  });
+});
