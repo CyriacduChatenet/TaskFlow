278421f068839dfb4f607bd758788ff72849a9e0
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-01-04 17:59:02 +0100

    fix(api): change user repository relation

--- api/src/user/user.repository.ts
+++ api/src/user/user.repository.ts
@@ -26,7 +26,7 @@ export class UserRepository extends Repository<User> {
 
     const query = this.createQueryBuilder('user')
       .leftJoinAndSelect('user.tasks', 'tasks')
-      .leftJoinAndSelect('user.notificaions', 'notifications')
+      .leftJoinAndSelect('user.notifications', 'notifications')
       .leftJoinAndSelect('user.teams', 'teams');
 
     if (email) query.andWhere('user.email = :email', { email });
@@ -47,7 +47,7 @@ export class UserRepository extends Repository<User> {
   async findOneUserById(id: string): Promise<User> {
     return this.createQueryBuilder('user')
       .leftJoinAndSelect('user.tasks', 'tasks')
-      .leftJoinAndSelect('user.notificaions', 'notifications')
+      .leftJoinAndSelect('user.notifications', 'notifications')
       .leftJoinAndSelect('user.teams', 'teams')
       .where('user.id = :id', { id })
       .getOne();
@@ -56,7 +56,7 @@ export class UserRepository extends Repository<User> {
   async findOneUserByEmail(email: string): Promise<User> {
     return this.createQueryBuilder('user')
       .leftJoinAndSelect('user.tasks', 'tasks')
-      .leftJoinAndSelect('user.notificaions', 'notifications')
+      .leftJoinAndSelect('user.notifications', 'notifications')
       .leftJoinAndSelect('user.teams', 'teams')
       .where('user.email = :email', { email })
       .getOne();
