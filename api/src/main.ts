49cfafcf132b01d840fc5eceefcaa84c8af2fc84
CyriacduChatenet <cyriacduchatenet@icloud.com>
2024-03-16 13:43:08 +0100

    doc(api): init swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TaskFlow API')
+    .setDescription('The TaskFlow API')
+    .setVersion('1.0.0')
+    .addTag('TaskFlow')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/doc', app, document, {
+    customSiteTitle: 'TaskFlow API documentation',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/7658037?v=4&s=400',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   const corsOptions: CorsOptions = {
     origin: [`${process.env.CLIENT_APP_URL}`],
